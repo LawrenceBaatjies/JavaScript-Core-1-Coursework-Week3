1e0d77cf0011fa02f68ce7969172e4ee3e083d78
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-02-06 08:39:47 +0200

    D-array-filter complete

--- 1-exercises/D-array-filter/exercise.js
+++ 1-exercises/D-array-filter/exercise.js
@@ -8,7 +8,8 @@
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter((elem) => elem !== null && elem.length === 2); // Complete this statement
+
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
