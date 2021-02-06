e2f6ef293728f77c36112e01514de0648f23d9d7
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-02-06 11:17:40 +0200

    2-madatory-6-lane-namess
    completed

--- 2-mandatory/6-lane-names.js
+++ 2-mandatory/6-lane-names.js
@@ -6,7 +6,12 @@
   HINT: string and array methods that could be helpful (indexOf, filter)
 */
 
-function getLanes() {}
+function getLanes(name) {
+
+    var hasWordLane = name.filter(hasWordLane => hasWordLane.includes("Lane"));
+  
+      return hasWordLane;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
